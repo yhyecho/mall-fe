ad8983d48a7925b81fb7193eb591e12ced702dd0
Echo <yhyecho@gmail.com>
2017-06-26 17:28:24 +0800

    webpack initial

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/**
+ * @Author: Echo
+ * @Date:   2017-06-26T17:16:32+08:00
+ * @Last modified by:   Echo
+ * @Last modified time: 2017-06-26T17:26:33+08:00
+ */
+
+module.exports = {
+  entry: './src/page/index/app.js',
+  output: {
+    path: './dist',
+    filename: 'app.js'
+  }
+}
