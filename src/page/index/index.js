c20d32a6774dc5ef0b43a1343e81c0d65652e5d1
Echo <yhyecho@gmail.com>
2017-06-30 10:17:05 +0800

    add hogan.js

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,7 +2,7 @@
  * @Author: Echo
  * @Date:   2017-06-26T17:16:55+08:00
  * @Last modified by:   Echo
- * @Last modified time: 2017-06-28T11:22:55+08:00
+ * @Last modified time: 2017-06-28T14:51:04+08:00
  */
 
 
@@ -15,13 +15,20 @@
 // console.log('hello index')
 var _mm = require('util/mm.js');
 
-_mm.request({
-  url: 'http://localhost:4000/posts',
-  success: function(data, msg) {
-    console.log(data);
-    console.log(msg);
-  },
-  error: function(errMsg) {
-    console.log(errMsg);
-  }
-})
+// _mm.request({
+//   url: 'http://localhost:4000/posts',
+//   success: function(data, msg) {
+//     console.log(data);
+//     console.log(msg);
+//   },
+//   error: function(errMsg) {
+//     console.log(errMsg);
+//   }
+// })
+console.log(_mm.getUrlParam('test'))
+
+var html = '<div>{{data}}</div>'
+var data = {
+  data: 'hello'
+}
+console.log(_mm.renderHtml(html, data))
--- src/util/mm.js
+++ src/util/mm.js
@@ -2,10 +2,13 @@
  * @Author: Echo
  * @Date:   2017-06-28T10:31:08+08:00
  * @Last modified by:   Echo
- * @Last modified time: 2017-06-28T11:23:10+08:00
+ * @Last modified time: 2017-06-28T15:06:13+08:00
  */
 
-
+var Hogan = require('hogan.js')
+var conf = {
+  serverHost: ''
+}
 
 var _mm = {
   // 网路请求
@@ -35,9 +38,51 @@ var _mm = {
       }
     })
   },
+  // 获取服务器地址
+  getServerUrl: function(path) {
+    return conf.serverHost + path
+  },
+  // 获取url参数
+  getUrlParam: function(name) {
+    var reg         = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"),
+        queryString = window.location.href.split('?')[1] || '',
+        result      = queryString.match(reg);
+        console.log(window.location);
+    return result ? decodeURIComponent(result[2]) : null
+  },
+  // 渲染html模版
+  renderHtml: function(htmlTemplate, data) {
+    var template = Hogan.compile(htmlTemplate),
+        result   = template.render(data)
+    return result
+  },
+  // 成功提示
+  successTips: function(msg) {
+    alert(msg || '操作成功了！')
+  },
+  // 错误提示
+  errorTips: function(msg) {
+    alert(msg || '出错了！')
+  },
+  // 字段的验证，支持非空、手机、邮箱的判断
+  validate: function(value, type) {
+    var value = $.trim(value)
+    // 非空验证
+    if('require' === type) {
+      return !!value // 强转成布尔型
+    }
+    // 手机号验证
+    if('phone' === type) {
+      return /^1\d{10}$/.test(value)
+    }
+    // 邮箱格式验证
+    if('email' === type) {
+      return /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test(value)
+    }
+  },
   // 统一登录处理
   doLogin: function() {
-    window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href); // 处理url中的特殊字符
+    window.location.href = '#/login?redirect=' + encodeURIComponent(window.location.href) // 处理url中的特殊字符
   }
 }
 
